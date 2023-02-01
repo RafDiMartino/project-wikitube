adf85991536c19264da631a6620276f60871c6cf
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-01 17:32:14 +0000

    adding youtube and wikipedia  APIs to get a very basic mvp

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,36 +9,35 @@ $("#search-button").click(function(e) {
     e.preventDefault()
     search = $("#search-input").val();
     console.log(search)
-    // getYoutubeVideo()
-    // videoTest.empty()
     getWikiArticles()
+    wikipediaTest.empty()
+    getYoutubeVideo()
+    videoTest.empty()
 });
 
 // Function to get Youtube videos
-// function getYoutubeVideo(){
-//     var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ youtubeAPI +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube&order=viewCount"
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         error: () => {
-//             alert("error")
-//             return
-//         },
-//     }).then(function(data) {
-//         console.log(data)
-//         for (let i = 0; i < data.items.length; i++) {
-//             var videoId = data.items[i].id.videoId;
-//             console.log(videoId)
-//             videoTest.append(`
-//                 <iframe width="420" height="315"
-//                     src="https://www.youtube.com/embed/${videoId}">
-//                 </iframe>
-//             `);
-//         }
-//     });
-    
-// }
-
+function getYoutubeVideo(){
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ youtubeAPI +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube&order=viewCount"
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: () => {
+            alert("error")
+            return
+        },
+    }).then(function(youtubeData) {
+        console.log(youtubeData)
+        for (let i = 0; i < youtubeData.items.length; i++) {
+            var videoId = youtubeData.items[i].id.videoId;
+            console.log(videoId)
+            videoTest.append(`
+                <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/${videoId}">
+                </iframe>
+            `);
+        }
+    });   
+}
 
 //Function to get Wikipedia articles
 function getWikiArticles(){
