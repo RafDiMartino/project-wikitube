92290d3bfe8d63d3aff2088be10f5e55c01daab1
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-01 16:53:07 +0000

    testing wikipedia api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const youtubeAPI = "AIzaSyBEBV7uxSff-XFwRYo2JGTFcXbAWFNtwX4"
 
 var videoTest = $("#youtube-test")
+var wikipediaTest = $("#wikipedia-test")
 var search = "";
 
 // Event listener to get the search value
@@ -8,13 +9,40 @@ $("#search-button").click(function(e) {
     e.preventDefault()
     search = $("#search-input").val();
     console.log(search)
-    getYoutubeVideo()
-    videoTest.empty()
+    // getYoutubeVideo()
+    // videoTest.empty()
+    getWikiArticles()
 });
 
 // Function to get Youtube videos
-function getYoutubeVideo(){
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ youtubeAPI +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube&order=viewCount"
+// function getYoutubeVideo(){
+//     var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ youtubeAPI +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube&order=viewCount"
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//         error: () => {
+//             alert("error")
+//             return
+//         },
+//     }).then(function(data) {
+//         console.log(data)
+//         for (let i = 0; i < data.items.length; i++) {
+//             var videoId = data.items[i].id.videoId;
+//             console.log(videoId)
+//             videoTest.append(`
+//                 <iframe width="420" height="315"
+//                     src="https://www.youtube.com/embed/${videoId}">
+//                 </iframe>
+//             `);
+//         }
+//     });
+    
+// }
+
+
+//Function to get Wikipedia articles
+function getWikiArticles(){
+    var queryURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + search + "&format=json&origin=*"
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -22,17 +50,16 @@ function getYoutubeVideo(){
             alert("error")
             return
         },
-    }).then(function(data) {
-        console.log(data)
-        for (let i = 0; i < data.items.length; i++) {
-            var videoId = data.items[i].id.videoId;
-            console.log(videoId)
+    }).then(function(wikiData) {
+        console.log(wikiData);
+        console.log(wikiData.query.search[0].snippet)
+
+        for (let i = 0; i < wikiData.query.search.length; i++) {
+            var snippet = wikiData.query.search[i].snippet;
+            console.log(snippet)
             videoTest.append(`
-                <iframe width="420" height="315"
-                    src="https://www.youtube.com/embed/${videoId}">
-                </iframe>
+                <p>${wikiData.query.search[i].snippet}</p>
             `);
         }
     });
-    
 }
