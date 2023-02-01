fd7d0512396936ac5792edd52c37afe49e1da30b
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-01 13:56:22 +0000

    testing youtube api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Hello Guys")
+const API = "AIzaSyBEBV7uxSff-XFwRYo2JGTFcXbAWFNtwX4"
+
+var videoTest = $("#youtube-test")
+
+
+// Function to get the current city weather
+function getYoutubeVideo(){
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ API +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube"
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: () => {
+            alert("error")
+            return
+        },
+    }).then(function(data) {
+        console.log(data)
+        for (let i = 0; i < data.items.length; i++) {
+            var videoId = data.items[i].id.videoId;
+            console.log(videoId)
+            videoTest.append(`
+                <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/${videoId}">
+                </iframe>
+            `);
+        }
+    });
+    
+}
+
+getYoutubeVideo()
