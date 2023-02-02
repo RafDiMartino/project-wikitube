d50563d821a3b1191c54c8ff67865e52514cd577
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-02 20:41:00 +0000

    adding modal component

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// const youtubeAPI = "AIzaSyARAdM68mQB0klzmy4LNFuo2e8Z4t4BQu8"
+const youtubeAPI = "AIzaSyARAdM68mQB0klzmy4LNFuo2e8Z4t4BQu8"
 
 var videoTest = $("#youtube-test")
 var wikipediaTest = $("#wikipedia-test")
@@ -7,10 +7,8 @@ var search = "";
 //Modal
 $('#exampleModal').modal('show');
 
-
 // Event listener to get the search value
 $(".search-button-class").click(function(e) {
-    
     e.preventDefault()
     search = $("#search-input").val();
     // $('#container').toggleClass("hide");
@@ -18,46 +16,44 @@ $(".search-button-class").click(function(e) {
     getWikiArticles()
     wikipediaTest.empty()
     
-    // getYoutubeVideo()
-    // videoTest.empty()
+    getYoutubeVideo()
+    videoTest.empty()
 });
 
 $(".search-button-modal").click(function(e) {
-    
     e.preventDefault()
     search = $("#search-input-modal").val();
-    // $('#container').toggleClass("hide");
+    $("div#container").toggleClass("hide");
     console.log(search)
     getWikiArticles()
     wikipediaTest.empty()
-    
-    // getYoutubeVideo()
-    // videoTest.empty()
+    getYoutubeVideo()
+    videoTest.empty()
 });
 
 // Function to get Youtube videos
-// function getYoutubeVideo(){
-//     var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ youtubeAPI +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube&order=viewCount"
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         error: () => {
-//             alert("error")
-//             return
-//         },
-//     }).then(function(youtubeData) {
-//         console.log(youtubeData)
-//         for (let i = 0; i < youtubeData.items.length; i++) {
-//             var videoId = youtubeData.items[i].id.videoId;
-//             console.log(videoId)
-//             videoTest.append(`
-//                 <iframe width="420" height="315"
-//                     src="https://www.youtube.com/embed/${videoId}">
-//                 </iframe>
-//             `);
-//         }
-//     });   
-// }
+function getYoutubeVideo(){
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?key="+ youtubeAPI +"&q="+ search +"&type=video&part=snippet&videoEmbeddable=true&videoSyndicated=true&videoLicense=youtube&order=viewCount"
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: () => {
+            alert("error")
+            return
+        },
+    }).then(function(youtubeData) {
+        console.log(youtubeData)
+        for (let i = 0; i < youtubeData.items.length; i++) {
+            var videoId = youtubeData.items[i].id.videoId;
+            console.log(videoId)
+            videoTest.append(`
+                <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/${videoId}">
+                </iframe>
+            `);
+        }
+    });   
+}
 
 //Function to get Wikipedia articles
 function getWikiArticles(){
